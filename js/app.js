e76247714147d75b6980a6795e08a5c90973e35a
LE TAN LOC <letanlocjp1001@gmail.com>
2022-01-26 01:00:13 +0900

    update reponsive

--- js/app.js
+++ js/app.js
@@ -4,3 +4,13 @@ window.addEventListener('scroll', () => {
 })
 
 // ********** FIXED NAVBAR ************
+$(document).ready(function () {
+  $('#menu-bar').click(function () {
+    $('#menu-bar').toggleClass('fa-times')
+    $('.menu').toggleClass('menu-toggle')
+  })
+  $(window).on('scroll load', function () {
+    $('#menu-bar').removeClass('fa-times')
+    $('.menu').removeClass('menu-toggle')
+  })
+})
